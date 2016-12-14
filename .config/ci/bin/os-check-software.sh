b9de26e271ba06c6ca22b1c08c20882a2c3996c8
Piotr Żuralski <piotr.zuralski@gmail.com>
2016-12-14 21:23:00 +0000

    Update os-check-software.sh

--- .config/ci/bin/os-check-software.sh
+++ .config/ci/bin/os-check-software.sh
@@ -5,6 +5,6 @@ export DEBIAN_FRONTEND=noninteractive;
 uname -a;
 
 apt-get update -y 2>&1;
-apt-get install openssh-client wget git openssl libssl-dev libcurl4-openssl-dev build-dep -y 2>&1;
+apt-get install openssh-client wget git openssl libssl-dev libcurl4-openssl-dev -y 2>&1;
 
 unset DEBIAN_FRONTEND;
